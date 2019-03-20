07a3ba6b1abcd85f620be99e5089ccbf5f7bcbbb
Babis Chalios <babis.chalios@bsc.es>
2019-03-20 11:49:49 +0100

    Adding basic packaging support

---
+++ README.md
@@ -0,0 +1,4 @@
+# Experiments
+
+Experiments is a Python package for setting up and running experiments for OmpSs-2
+Cluster and MPI applications
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+        name="experiments",
+        version="0.0.1",
+        author="Babis Chalios",
+        author_email="babis.chalios@bsc.es",
+        description="A Python package for running experiments on BSC machines",
+        long_description=long_description,
+        long_description_content_type="test/markdown",
+        url="git@github.com:bchalios/experiments.git",
+        packages=setuptools.find_packages(),
+)
